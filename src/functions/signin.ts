561a4f28a93892f07cd191ea620b02ff070fe009
Bruno Miguel Groth <bruno.groth@hotmail.com>
2025-07-19 10:47:06 -0300

    fix signin

--- src/functions/signin.ts
+++ src/functions/signin.ts
@@ -1,8 +1,14 @@
-export async function handler(event) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            'message': 'Vamos fazer login',
-        }),
-    }
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { SignInController } from "../controllers/SignInController";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+    const body = JSON.parse(event.body ?? '{}');
+    const params = event.pathParameters ?? {};
+    const queryParams = event.queryStringParameters ?? {};
+
+    await SignInController.handle({
+        body,
+        params,
+        queryParams
+    });
 }
